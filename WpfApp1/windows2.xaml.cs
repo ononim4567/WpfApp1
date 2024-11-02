3af1f994754e684a8759e05bddfc6fa658d992c0
Лысенко Сергей <sergei047@stud.vvsu.ru>
2024-11-02 14:08:41 +1000

    Добавьте файлы проекта.

---
+++ WpfApp1/windows2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для windows2.xaml
+    /// </summary>
+    public partial class windows2 : Window
+    {
+        public windows2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Close();
+        }
+    }
+}
