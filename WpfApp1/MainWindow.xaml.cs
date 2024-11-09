6580f4da20705ace196ef9cc9ab63fa1b7207358
Сергей Лысенко <liysenk0556@mail.ru>
2024-11-09 15:02:30 +1000

    add menu ²ьте файлы проекта.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string imgsorcel1 = @"C:\img2.akspic.ru-bak-masiny-world_of_tanks-boevaya_mashina-vid_transporta-2560x1600.jpg";
+        private string imgsorcel2 = @"C:\img1.akspic.ru-voennosluzhashhie-polet-aviaciya-vozdushnye_sily-aerokosmicheskaya_tehnika-3543x2362.jpg";
         public MainWindow()
         {
             InitializeComponent();
@@ -30,6 +32,22 @@ namespace WpfApp1
             windows2 window = new windows2();
             window.Show();
             this.Close();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("приложение закрылос");
+            Application.Current.Shutdown();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            DisplayImage.Source = new BitmapImage(new Uri(imgsorcel1));
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            DisplayImage.Source = new BitmapImage(new Uri(imgsorcel2));
         }
     }
 }
